77100856e0fab66cc768b52c0ed0c1d87cb9ff19
Carla Taiany Oliveira <106335055+CarlaTaiany@users.noreply.github.com>
2025-05-30 15:09:41 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Semaforo
+{
+    partial class Semafaro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Semafaro));
+            this.pibSemafaro = new System.Windows.Forms.PictureBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.tmrTempo = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pibSemafaro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pibSemafaro
+            // 
+            this.pibSemafaro.Image = global::Semaforo.Properties.Resources.semafDesligado;
+            this.pibSemafaro.Location = new System.Drawing.Point(45, 44);
+            this.pibSemafaro.Name = "pibSemafaro";
+            this.pibSemafaro.Size = new System.Drawing.Size(285, 356);
+            this.pibSemafaro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibSemafaro.TabIndex = 0;
+            this.pibSemafaro.TabStop = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.btnIniciar.Font = new System.Drawing.Font("Schadow BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.White;
+            this.btnIniciar.Location = new System.Drawing.Point(45, 430);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(132, 65);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.btnParar.Font = new System.Drawing.Font("Schadow BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParar.ForeColor = System.Drawing.Color.White;
+            this.btnParar.Location = new System.Drawing.Point(198, 430);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(132, 65);
+            this.btnParar.TabIndex = 2;
+            this.btnParar.Text = "PARAR";
+            this.btnParar.UseVisualStyleBackColor = false;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // tmrTempo
+            // 
+            this.tmrTempo.Interval = 10000;
+            this.tmrTempo.Tick += new System.EventHandler(this.tmrTempo_Tick);
+            // 
+            // Semafaro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.ClientSize = new System.Drawing.Size(378, 531);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pibSemafaro);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Semafaro";
+            this.Text = "Semáfaro";
+            ((System.ComponentModel.ISupportInitialize)(this.pibSemafaro)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pibSemafaro;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Timer tmrTempo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semaforo
+{
+    public partial class Semafaro : Form
+    {
+        // Variável para controlar o estado atual do semáforo
+        private int estadoSemafaro = 0; // 0: Desligado, 1: Vermelho, 2: Verde, 3: Amarelo
+
+        public Semafaro()
+        {
+            InitializeComponent();
+            // Garante que a imagem inicial seja a desligada
+            pibSemafaro.Image = Properties.Resources.semafDesligado;
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            // Começa no vermelho quando inicia
+            estadoSemafaro = 1;
+            pibSemafaro.Image = Properties.Resources.semafVermelho;
+            tmrTempo.Start(); // Inicia o Timer
+
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            tmrTempo.Stop(); // Para o Timer
+            estadoSemafaro = 0; // Volta para o estado desligado
+            pibSemafaro.Image = Properties.Resources.semafDesligado;
+        }
+
+        private void tmrTempo_Tick(object sender, EventArgs e)
+        {
+            // Lógica para trocar as cores do semáforo
+            estadoSemafaro++; // Avança para o próximo estado
+
+            // Se o estado for maior que 3 (amarelo), volta para 1 (vermelho)
+            if (estadoSemafaro > 3)
+            {
+                estadoSemafaro = 1;
+            }
+
+            // Define a imagem com base no estado atual
+            switch (estadoSemafaro)
+            {
+                case 1: // Vermelho
+                    pibSemafaro.Image = Properties.Resources.semafVermelho;
+                    break;
+                case 2: // Amarelo
+                    pibSemafaro.Image = Properties.Resources.semafVerde;
+                    break;
+                case 3: // Verde
+                    pibSemafaro.Image = Properties.Resources.semafAmarelo;
+                    break;
+            }
+        }
+    }
+}
